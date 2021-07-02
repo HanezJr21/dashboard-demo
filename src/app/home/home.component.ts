152ed5539f2eb7f7dc5cb64ab28d9236662361c3
adhetiga19 <adhetiga19@gmail.com>
2021-07-02 15:39:08 +0700

    3 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,11 +3,14 @@ import { Component } from '@angular/core';
 import { User } from '../_models';
 import { AccountService } from '../_services';
 
-@Component({ templateUrl: 'home.component.html' })
+@Component({
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.css']
+})
 export class HomeComponent {
-    user: User;
+  user: User;
 
-    constructor(private accountService: AccountService) {
-        this.user = this.accountService.userValue;
-    }
+  constructor(private accountService: AccountService) {
+    this.user = this.accountService.userValue;
+  }
 }
